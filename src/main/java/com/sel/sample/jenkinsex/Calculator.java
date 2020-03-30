f9fa9f746d0f681904c8773a253c103dd75d85d4
itexps <itexps@192.168.0.34>
2020-03-30 15:56:15 -0500

    Add Tests

--- src/main/java/com/sel/sample/jenkinsex/Calculator.java
+++ src/main/java/com/sel/sample/jenkinsex/Calculator.java
@@ -9,7 +9,7 @@ package com.sel.sample.jenkinsex;
  *
  * @author itexps
  */
-public class Caluclator {
+public class Calculator {
 
     public double add(double n1,double n2){
         return n1+n2;
---
+++ src/test/java/com/sel/sample/jenkinsex/NewEmptyJUnitTest.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sel.sample.jenkinsex;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author itexps
+ */
+public class NewEmptyJUnitTest {
+    Calculator c;
+    public NewEmptyJUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        c=new Calculator();
+    }
+    
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void testAdd(){
+        double result=c.add(20, 40);
+        assertEquals(60, result,0.0);
+    }
+    @Test
+    public void testSubtract(){
+      double result=c.add(40, 20);
+      assertEquals(20, result,0.0);  
+    }
+}
